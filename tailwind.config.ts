458ff3f17b52b785df569af3454d3da73c2e0c2b
programmeranamul <programmer.anamul@gmail.com>
2026-01-18 04:43:04 +0600

    Implement MDX Editor

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -46,12 +46,18 @@ const config: Config = {
         "space-grotesk": ["var(--font-space-grotesk)"],
       },
       borderRadius: {
+        "2": "8px",
+        "1.5": "6px",
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
       },
+      backgroundImage: {
+        "auth-dark": 'url("/images/auth-dark.png")',
+        "auth-light": 'url("/images/auth-light.png")',
+      },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
 };
 export default config;
